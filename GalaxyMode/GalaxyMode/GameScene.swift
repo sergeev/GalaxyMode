d26a034c78e3d0336ff734e1574d7e033eb476ff
sergeev <v.sergeev.m@gmail.com>
2019-04-14 01:20:56 +0700

    init base background and player in game ;)

--- GalaxyMode/GalaxyMode/GameScene.swift
+++ GalaxyMode/GalaxyMode/GameScene.swift
@@ -9,79 +9,64 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // Отвечает за заднее поле экрана background animation
+    var starfield:SKEmitterNode!
+    // Создаем игрока, он не анимация а значет SKSpriteNone!
+    var player:SKSpriteNode!
+    // Создаем переменую под вывод текста на экране
+    var scoreLabel:SKLabelNode!
+    
+    var score:Int = 0 {
+        // добавляем текст на экран
+        didSet {
+            scoreLabel.text = "Счет: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
+        // создаем переменную анимации заднего поля(звезд)
+        starfield = SKEmitterNode(fileNamed: "Starfield")
+        // создаем позицию поля, расположение: 1472 прокрывает площадь всех айфонов 6/7/8/10?
+        starfield.position = CGPoint(x: 0, y: 1472)
+        // создаем задержку для экрана, для отображения на экране всех звезд, а не первую анимацию появления
+        starfield.advanceSimulationTime(10)
+        // добавляем на экран
+        self.addChild(starfield)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // устанваливаем наш фон так, чтобы он был всегда последним с низу слоев
+        starfield.zPosition = -1
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // добавляем игрока
+        player = SKSpriteNode(imageNamed: "shuttle")
+        // указываем позицию игрока X: 0 - нулевая позиция экрана, Y: -300 - опущен на -300 по отношению к верхнему экрану
+        player.position = CGPoint(x: 0, y: -200)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        self.addChild(player)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
+        // добавление физики
+        // отключаем отключаем гравитацию
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        // отслеживаем соприкосновения в игре, добавляем в class GameScene SKPhysicsContactDelegate
+        self.physicsWorld.contactDelegate = self
+        
+        // Добавляем характеристики для scoreLabel
+        scoreLabel = SKLabelNode(text: "Счет: 0")
+        // Устанавливаем характеристики для шрифта можно найти по iOS fonts
+        scoreLabel.fontName = "AmericanTypewriter-Bold"
+        // устанавливаем размер шрифта
+        scoreLabel.fontSize = 36
+        // устанавливаем цвет шрифта  - белый
+        scoreLabel.fontColor = UIColor.white
+        // устанавливаем позицию текста, берем выстоту и ширину экрана self.frame.size.height и отнимаем 60
+        //scoreLabel.position = CGPoint(x: -200, y: self.frame.size.height - 100)
+        scoreLabel.position = CGPoint(x: -300, y: 625)
+        // обознаваем score как 0 при старте игры
+        score = 0
+        // Добавляем scoreLaberl на экран
+        self.addChild(scoreLabel)
+        }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
